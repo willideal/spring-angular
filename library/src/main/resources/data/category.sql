993a5db9cf404580c2934d6b26b97c7ca53f89f1
Noutai Wilfrid Cesaire <wilfrid-cesaire.noutai@etu.uni-orleans.fr>
2020-01-17 15:09:59 +0100

    add database source

---
+++ library/src/main/resources/data/category.sql
@@ -0,0 +1,14 @@
+insert into category values ('INF', 'Informatique');
+insert into category values ('MAT', 'Mathématiques, Physiques et Chimie');
+insert into category values ('ALB', 'Albums');
+insert into category values ('BDE', 'Bandes dessinées');
+insert into category values ('CON', 'Contes');
+insert into category values ('POE', 'Poésie');
+insert into category values ('ROM', 'Romans et récits illustrés');
+insert into category values ('THE', 'Théâtre');
+insert into category values ('PHI', 'Philosophie');
+insert into category values ('SCI', 'Sciences de la vie et de la terre');
+insert into category values ('POL', 'Politique');
+insert into category values ('SOC', 'Sociologie');
+insert into category values ('ECO', 'Economie et Finance');
+insert into category values ('JOU', 'Journal, Magazine');
